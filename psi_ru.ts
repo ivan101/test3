821f8c966e54c1559775605dcb45372e847a652e
ivan101 <ivan101@mail.ru>
2008-11-18 11:05:16 +0000

    fix issue #1

--- psi_ru.ts
+++ psi_ru.ts
@@ -128,7 +128,7 @@ Copyright © 2001-2008 The Psi Team.&lt;br&gt;</translation>
 Join the translation team today! Go to 
 &lt;a href=&quot;http://forum.psi-im.org/forum/14&quot;&gt;
 http://forum.psi-im.org/forum/14&lt;/a&gt; for further details!</source>
-        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©14.11.2008</translation>
+        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©18.11.2008</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.cpp" line="70"/>
@@ -5117,7 +5117,7 @@ Reason: %1</source>
     <message>
         <location filename="../src/miniclient.cpp" line="166"/>
         <source>&amp;Details...</source>
-        <translation>&amp;Подробности...</translation>
+        <translation>&amp;Подробнее...</translation>
     </message>
     <message>
         <location filename="../src/miniclient.cpp" line="165"/>
@@ -5127,7 +5127,7 @@ Reason: %1</source>
     <message>
         <location filename="../src/miniclient.cpp" line="164"/>
         <source>Server Authentication</source>
-        <translation>Аутентификация сервера</translation>
+        <translation>Аутентификация</translation>
     </message>
     <message>
         <location filename="../src/miniclient.cpp" line="262"/>
@@ -5525,7 +5525,7 @@ Details: %1</source>
     <message>
         <location filename="../src/options/opt_chat.ui" line="32"/>
         <source>Alert chat messages even if chat window is open</source>
-        <translation>Уведомлять о новых репликах в уже открытом окне чата</translation>
+        <translation>Уведомлять о событиях в открытых окнах</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="120"/>
@@ -5545,12 +5545,12 @@ Details: %1</source>
     <message>
         <location filename="../src/options/opt_chat.ui" line="156"/>
         <source>Delete chat window contents</source>
-        <translation>Очистить окно чата</translation>
+        <translation>Очищать окно чата</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="25"/>
         <source>Enter key sends chat messages</source>
-        <translation>Клавиша Enter используется для отправки сообщений</translation>
+        <translation>Отправлять сообщения клавишей Enter</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="87"/>
@@ -5585,17 +5585,17 @@ Details: %1</source>
     <message>
         <location filename="../src/options/opt_chat.ui" line="182"/>
         <source>One day after chat window was closed</source>
-        <translation>Через день после закрытия окна чата</translation>
+        <translation>Через день после закрытия</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="175"/>
         <source>One hour after chat window was closed</source>
-        <translation>Через час после закрытия окна чата</translation>
+        <translation>Через час после закрытия</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="46"/>
         <source>Open chats in compact mode</source>
-        <translation>Открывать чаты в компактном режиме</translation>
+        <translation>Компактное окно чатов</translation>
     </message>
     <message>
         <location filename="../src/options/opt_chat.ui" line="13"/>
@@ -5615,7 +5615,7 @@ Details: %1</source>
     <message>
         <location filename="../src/options/opt_chat.ui" line="168"/>
         <source>When chat window is closed</source>
-        <translation>При закрытии окна чата</translation>
+        <translation>При закрытии окна</translation>
     </message>
 </context>
 <context>
@@ -6437,7 +6437,7 @@ Option names may not be empty, end in &apos;.&apos; or contain &apos;..&apos;.</
     <message>
         <location filename="../src/options/opt_events.cpp" line="26"/>
         <source>The events behaviour</source>
-        <translation>Настройка поведения событий</translation>
+        <translation>Настройка событий</translation>
     </message>
     <message>
         <location filename="../src/options/opt_events.cpp" line="57"/>
@@ -7661,7 +7661,7 @@ Check the &lt;i&gt;Enable Emoticons&lt;/i&gt; checkbox if you&apos;d like text s
     <message>
         <location filename="../src/psiaccount.cpp" line="1311"/>
         <source>&amp;Details...</source>
-        <translation>&amp;Подробности...</translation>
+        <translation>&amp;Подробнее...</translation>
     </message>
     <message>
         <location filename="../src/psiaccount.cpp" line="947"/>
@@ -7821,7 +7821,7 @@ Check the &lt;i&gt;Enable Emoticons&lt;/i&gt; checkbox if you&apos;d like text s
     <message>
         <location filename="../src/psiaccount.cpp" line="1309"/>
         <source>Server Authentication</source>
-        <translation>Аутентификация сервера</translation>
+        <translation>Аутентификация</translation>
     </message>
     <message>
         <location filename="../src/psiaccount.cpp" line="1578"/>
@@ -9830,17 +9830,17 @@ Would you like to activate that session now?</source>
     <message>
         <location filename="../src/Certificates/CertificateErrorDialog.cpp" line="20"/>
         <source>&amp;Details...</source>
-        <translation>&amp;Подробности...</translation>
+        <translation>&amp;Подробнее...</translation>
     </message>
     <message>
         <location filename="../src/Certificates/CertificateErrorDialog.cpp" line="21"/>
         <source>&amp;Connect anyway</source>
-        <translation>&amp;Подключиться все равно</translation>
+        <translation>&amp;Игнорировать</translation>
     </message>
     <message>
         <location filename="../src/Certificates/CertificateErrorDialog.cpp" line="22"/>
         <source>&amp;Trust this certificate</source>
-        <translation>&amp;Доверять этому сертификату</translation>
+        <translation>&amp;Доверять</translation>
     </message>
     <message>
         <location filename="../src/activitycatalog.cpp" line="75"/>
