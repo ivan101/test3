8b43f75e79e59fb83fe0e3e5bddf7e82b997b0b7
ivan101 <ivan101@mail.ru>
2008-11-21 07:09:39 +0000

    updated moods

--- psi_ru.ts
+++ psi_ru.ts
@@ -128,7 +128,7 @@ Copyright © 2001-2008 The Psi Team.&lt;br&gt;</translation>
 Join the translation team today! Go to 
 &lt;a href=&quot;http://forum.psi-im.org/forum/14&quot;&gt;
 http://forum.psi-im.org/forum/14&lt;/a&gt; for further details!</source>
-        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©18.11.2008</translation>
+        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©21.11.2008</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.cpp" line="70"/>
@@ -5514,6 +5514,11 @@ Details: %1</source>
         <source>Check for updates on startup</source>
         <translation>Проверка обновлений при запуске</translation>
     </message>
+    <message>
+        <location filename="../src/options/opt_application.ui" line="66"/>
+        <source>Disable scrollbar</source>
+        <translation>Отключить полосу прокрутки</translation>
+    </message>
 </context>
 <context>
     <name>OptChat</name>
@@ -6307,6 +6312,11 @@ Option names may not be empty, end in &apos;.&apos; or contain &apos;..&apos;.</
         <location filename="../src/options/opt_application.cpp" line="62"/>
         <source>Starts Psi with only the docklet icon visible.</source>
         <translation>Запускать Psi с иконкой в трее</translation>
+    </message>
+    <message>
+        <location filename="../src/options/opt_application.cpp" line="53"/>
+        <source>Disables scrollbar in the application window.</source>
+        <translation>Отключает полосу прокрутки в окне приложения.</translation>
     </message>
 </context>
 <context>
@@ -9030,7 +9040,7 @@ Would you like to activate that session now?</source>
     <message>
         <location filename="../src/moodcatalog.cpp" line="65"/>
         <source>Bored</source>
-        <translation>Cкучающее</translation>
+        <translation>Скучающее</translation>
     </message>
     <message>
         <location filename="../src/moodcatalog.cpp" line="66"/>
@@ -10196,6 +10206,101 @@ Would you like to activate that session now?</source>
         <location filename="../src/userlist.cpp" line="503"/>
         <source>Activity</source>
         <translation>Занятие</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="72"/>
+        <source>Amorous</source>
+        <translation>Влюбчивое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="80"/>
+        <source>Cautious</source>
+        <translation>Осторожное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="82"/>
+        <source>Confident</source>
+        <translation>Уверенное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="84"/>
+        <source>Contemplative</source>
+        <translation>Задумчивое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="87"/>
+        <source>Crazy</source>
+        <translation>Сумасшедшее</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="88"/>
+        <source>Creative</source>
+        <translation>Творческое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="90"/>
+        <source>Dejected</source>
+        <translation>Удрученное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="94"/>
+        <source>Dismayed</source>
+        <translation>Встревоженное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="97"/>
+        <source>Envious</source>
+        <translation>Завистливое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="104"/>
+        <source>Hopeful</source>
+        <translation>Обнадеженное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="119"/>
+        <source>Lucky</source>
+        <translation>Удачливое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="125"/>
+        <source>Outraged</source>
+        <translation>Разбитое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="128"/>
+        <source>Relaxed</source>
+        <translation>Расслабленное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="139"/>
+        <source>Spontaneous</source>
+        <translation>Спонтанное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="141"/>
+        <source>Strong</source>
+        <translation>Сильное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="143"/>
+        <source>Thankful</source>
+        <translation>Благодарственное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="145"/>
+        <source>Tired</source>
+        <translation>Усталое</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="146"/>
+        <source>Else</source>
+        <translation>Неопределенное</translation>
+    </message>
+    <message>
+        <location filename="../src/moodcatalog.cpp" line="147"/>
+        <source>Weak</source>
+        <translation>Слабое</translation>
     </message>
 </context>
 <context>
