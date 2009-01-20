c51398a26c37ed9865fb8514d6c1cd55ebc5a725
ivan101 <ivan101@mail.ru>
2009-01-20 17:48:45 +0000

    Updated

--- psi_ru.ts
+++ psi_ru.ts
@@ -128,7 +128,7 @@ Copyright © 2001-2008 The Psi Team.&lt;br&gt;</translation>
 Join the translation team today! Go to 
 &lt;a href=&quot;http://forum.psi-im.org/forum/14&quot;&gt;
 http://forum.psi-im.org/forum/14&lt;/a&gt; for further details!</source>
-        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©13.01.2009</translation>
+        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©20.01.2009</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.cpp" line="70"/>
@@ -5819,102 +5819,102 @@ Details: %1</source>
     <message>
         <location filename="../src/options/opt_roster.ui" line="14"/>
         <source>OptApplicationUI</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="26"/>
         <source>Roster window always on top</source>
-        <translation type="unfinished">Окно ростера всегда наверху</translation>
+        <translation>Окно ростера всегда наверху</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="33"/>
         <source>Auto-resize contact list</source>
-        <translation type="unfinished">Автоматически изменять размер ростера</translation>
+        <translation>Автоматически изменять размер ростера</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="43"/>
         <source>Use left-click for menu</source>
-        <translation type="unfinished">Вызов меню левой кнопкой мыши</translation>
+        <translation>Вызов меню левой кнопкой мыши</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="50"/>
         <source>Show menubar</source>
-        <translation type="unfinished">Отображать панель меню в ростере</translation>
+        <translation>Отображать панель меню в ростере</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="57"/>
         <source>Disables scrollbar in the application window.</source>
-        <translation type="unfinished">Отключает полосу прокрутки в окне приложения.</translation>
+        <translation>Отключает полосу прокрутки в окне приложения.</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="60"/>
         <source>Disable scrollbar</source>
-        <translation type="unfinished">Отключить полосу прокрутки</translation>
+        <translation>Отключить полосу прокрутки</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="67"/>
         <source>Icons</source>
-        <translation type="unfinished"></translation>
+        <translation>Иконки</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="79"/>
         <source>Show avatars in roster</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать аватары в ростере</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="82"/>
         <source>Avatars</source>
-        <translation type="unfinished"></translation>
+        <translation>Аватары</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="89"/>
         <source>Show moods in roster</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать настроения в ростере</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="92"/>
         <source>Moods</source>
-        <translation type="unfinished">Настроения</translation>
+        <translation>Настроения</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="99"/>
         <source>Show icon when contact is listening to music</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать иконку когда контакт слушает музыку</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="102"/>
         <source>Now playing</source>
-        <translation type="unfinished"></translation>
+        <translation>Слушает</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="109"/>
         <source>Show icon of user client</source>
-        <translation type="unfinished"></translation>
+        <translation>Показывать иконки клиентов</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="112"/>
         <source>Clients</source>
-        <translation type="unfinished"></translation>
+        <translation>Клиенты</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="125"/>
         <source>Avatar settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройки аватар</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="133"/>
         <source>Fixed height</source>
-        <translation type="unfinished"></translation>
+        <translation>Фиксированная высота</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="161"/>
         <source>Width not bigger than row height</source>
-        <translation type="unfinished"></translation>
+        <translation>Ширина не больше чем высота строки</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.ui" line="174"/>
         <source>Round corners</source>
-        <translation type="unfinished"></translation>
+        <translation>Круглые уголки</translation>
     </message>
 </context>
 <context>
@@ -6701,32 +6701,32 @@ Option names may not be empty, end in &apos;.&apos; or contain &apos;..&apos;.</
     <message>
         <location filename="../src/options/opt_roster.cpp" line="20"/>
         <source>Roster</source>
-        <translation type="unfinished">Список контактов</translation>
+        <translation>Ростер</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.cpp" line="20"/>
         <source>Roster window options</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройка окна списка контактов</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.cpp" line="38"/>
         <source>Makes the main Psi window always be in front of other windows.</source>
-        <translation type="unfinished">Помещать основное окно Psi поверх всех других окон.</translation>
+        <translation>Помещать основное окно Psi поверх всех других окон.</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.cpp" line="40"/>
         <source>Makes the main Psi window resize automatically to fit all contacts.</source>
-        <translation type="unfinished">Автоматически изменять размер основного окна для отображения всех контактов.</translation>
+        <translation>Автоматически изменять размер основного окна для отображения всех контактов.</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.cpp" line="43"/>
         <source>Normally, right-clicking with the mouse on a contact will activate the context-menu.  Check this option if you&apos;d rather use a left-click.</source>
-        <translation type="unfinished">Обычно при клике правой кнопкой мыши по контакту открывается контекстное меню.  Отметьте эту опцию, если хотите использовать для этого левую кнопку.</translation>
+        <translation>Обычно при клике правой кнопкой мыши по контакту открывается контекстное меню.  Отметьте эту опцию, если хотите использовать для этого левую кнопку.</translation>
     </message>
     <message>
         <location filename="../src/options/opt_roster.cpp" line="45"/>
         <source>Shows the menubar in the application window.</source>
-        <translation type="unfinished">Показывать меню в окне приложения.</translation>
+        <translation>Показывать меню в окне приложения.</translation>
     </message>
 </context>
 <context>
